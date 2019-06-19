d7991259f3c42c8edb8917ef57e5f6c219ebef8c
Kevin Saucedo <user002_@outlook.com>
2019-06-18 22:06:01 -0500

    Bases de DAL,BL

---
+++ src/datos/EmployeeDAL.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+
+import entidades.Employee;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author adriana
+ */
+public class EmployeeDAL {
+
+    public static String insertEmployees(Employee employee) {
+        return null;
+    }
+
+    public static ArrayList<Employee> listEmployes() {
+        return null;
+    }
+
+    public static String searchEmployeesForCode(double emp_no) {
+        return null;
+    }
+
+    public static String updateEmployees() {
+        return null;
+    }
+}
---
+++ src/logica/EmployeeBL.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+import entidades.Employee;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author adriana
+ */
+public class EmployeeBL {
+
+    public static String insertEmployees(Employee employee) {
+        return null;
+    }
+
+    public static ArrayList<Employee> listEmployes() {
+        return null;
+    }
+
+    public static String searchEmployeesForCode(double emp_no) {
+        return null;
+    }
+
+    public static String updateEmployees() {
+        return null;
+    }
+
+}
